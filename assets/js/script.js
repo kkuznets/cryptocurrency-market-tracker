c02f08bfcb86855600a7d35d6340e4d069548094
Prakriti <rajbhandariprakriti.7@gmail.com>
2020-04-02 19:35:45 +1100

    updated convert btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,30 @@
-$(document).ready(function () {
-  /* $("#currencyList").on("change", function() {
-    alert(
-      $(this)
-        .find("option:selected")
-        .attr("id")
-    );
-  });*/
-  function convert(event) {
-    var keycode = event.keyCode ? event.keyCode : event.which;
-    if (keycode == "13") {
-      var input = $("#inputGroup").find(':selected').attr('value');
-      var queryURL =
-        "https://min-api.cryptocompare.com/data/price?fsym=" + input + "&tsyms=USD,EUR,AUD" +
-        "&api_key=4032fc9713c205be9d015deab9bbbce1bb323fc207e32e773df9be829a3aa93f";
-      $.ajax({
-        url: queryURL,
-        dataType: "json",
-        method: "GET"
-      }).then(function (response) {
-        var selectedCurrency = $("#currencyList")
-          .children("option:selected")
-          .val();
+$(document).ready(function() {
+  $("#convertBtn").on("click", function() {
+    var input = $("#inputGroup")
+      .find(":selected")
+      .attr("value");
+    var queryURL =
+      "https://min-api.cryptocompare.com/data/price?fsym=" +
+      input +
+      "&tsyms=USD,EUR,AUD" +
+      "&api_key=4032fc9713c205be9d015deab9bbbce1bb323fc207e32e773df9be829a3aa93f";
+    $.ajax({
+      url: queryURL,
+      dataType: "json",
+      method: "GET"
+    }).then(function(response) {
+      var selectedCurrency = $("#currencyList")
+        .children("option:selected")
+        .val();
 
-        //console.log(response.USD);
-        var coinAmount = parseFloat($("#btcInput").val());
-        if (selectedCurrency == 1) {
-          //usdValue = parseInt(response.USD);
-          $("#currencyDisplay").val(parseFloat(response.USD) * coinAmount);
-        } else if (selectedCurrency == 2) {
-          $("#currencyDisplay").val(parseFloat(response.AUD) * coinAmount);
-        } else {
-          $("#currencyDisplay").val(parseFloat(response.EUR) * coinAmount);
-        }
-      });
-    }
-  }
-
-
-  $("#btcInput").keypress(function (event) {
-    //event.preventDefault();
-    convert(event);
+      var coinAmount = parseFloat($("#btcInput").val());
+      if (selectedCurrency == 1) {
+        $("#currencyDisplay").val(parseFloat(response.USD) * coinAmount);
+      } else if (selectedCurrency == 2) {
+        $("#currencyDisplay").val(parseFloat(response.AUD) * coinAmount);
+      } else {
+        $("#currencyDisplay").val(parseFloat(response.EUR) * coinAmount);
+      }
+    });
   });
-
-  $("#convert").click(function (event) {
-    convert(event);
-  });
-
-}
-);
+});
