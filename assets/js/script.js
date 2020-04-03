d8c29c2ce8fd60ef5eedc897a668f4b444834f95
Kirill Kuznetsov <yours.kkuznets@gmail.com>
2020-04-03 14:29:30 +1100

    Updated converter style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,35 @@
 $(document).ready(function () {
-  function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute("class", type);
-  }
 
   $("#convertBtn").on("click", function () {
     if (!$("#btcInput").val()) {
       $("#btcInput").attr("placeholder", "Please, fill out this field!");
       return;
     }
-    var input = $("#inputGroup")
-      .find(":selected")
-      .attr("value");
-    var output = $("#currencyList")
-      .find(":selected")
-      .attr("value");
-    var queryURL =
-      "https://min-api.cryptocompare.com/data/price?fsym=" +
-      input +
-      "&tsyms=" + output +
-      "&api_key=4032fc9713c205be9d015deab9bbbce1bb323fc207e32e773df9be829a3aa93f";
+    var input = $("#inputGroup").find(":selected").attr("value");
+    var output = $("#currencyList").find(":selected").attr("value");
+
     $.ajax({
-      url: queryURL,
+      url: "https://min-api.cryptocompare.com/data/price?fsym=" +
+        input +
+        "&tsyms=" +
+        output +
+        "&api_key=4032fc9713c205be9d015deab9bbbce1bb323fc207e32e773df9be829a3aa93f",
       dataType: "json",
       method: "GET"
     }).then(function (response) {
       var selectedCurrency = $("#currencyList")
         .children("option:selected")
         .val();
-
       var coinAmount = parseFloat($("#btcInput").val());
       $("#currencyDisplay").val(parseFloat(response[output]) * coinAmount);
-
-
     });
   });
 
   $("#copyBtn").on("click", function () {
-    /* Get the text field */
     var copyText = document.getElementById("currencyDisplay");
 
-    /* Select the text field */
     copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-    /* Copy the text inside the text field */
+    copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
-  }
-  )
+  });
 });
