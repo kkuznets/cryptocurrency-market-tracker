91cbf69ab234b4b8c22a9c651354d34704d14a68
Kirill Kuznetsov <yours.kkuznets@gmail.com>
2020-04-03 21:02:50 +1100

    Updated converter
    Users can now convert any currency on the list to any other currency on the list.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
 
+  VANTA.NET({
+    el: "#netBackground",
+    mouseControls: true,
+    touchControls: true,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xff9c6a,
+    backgroundColor: 0x131722
+  })
+
   $("#convertBtn").on("click", function () {
-    if (!$("#btcInput").val()) {
-      $("#btcInput").attr("placeholder", "Please, fill out this field!");
+    if (!$("#currencyInput").val()) {
+      $("#currencyOutput").val("");
+      $("#currencyInput").attr("placeholder", "Please, fill out this field!");
       return;
     }
-    var input = $("#inputGroup").find(":selected").attr("value");
+    var input = $("#currencyChoice").find(":selected").attr("value");
     var output = $("#currencyList").find(":selected").attr("value");
 
     $.ajax({
@@ -20,16 +31,114 @@ $(document).ready(function () {
       var selectedCurrency = $("#currencyList")
         .children("option:selected")
         .val();
-      var coinAmount = parseFloat($("#btcInput").val());
-      $("#currencyDisplay").val(parseFloat(response[output]) * coinAmount);
+      var coinAmount = parseFloat($("#currencyInput").val());
+      $("#currencyOutput").val(parseFloat(response[output]) * coinAmount);
     });
   });
 
   $("#copyBtn").on("click", function () {
-    var copyText = document.getElementById("currencyDisplay");
+    var copyText = document.getElementById("currencyOutput");
 
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
+  });
+
+
+
+  $(document).on("click", ".currency", function () {
+    $("#mainInfo").empty();
+    $("#mainInfo").append(`
+          <div class="row">
+            <p>
+              <h1 class="invisible"> Nothing
+              </h1>
+            </p>
+            <div class="col">
+            </div>
+          </div>
+          <div class="row mb-5">
+            <div class="col-lg-3 my-auto text-center">
+              <img id="img">
+            </div>
+            <div class="col-lg-4 my-auto mx-auto text-center">
+              <p>
+                <h2 id="coinTitle">
+                </h2>
+              </p>
+            </div>
+            <div class="col-lg-5 my-auto">
+              <h4 id="fcasRating" class="align-middle"></h4>
+              <h4 id="marketMaturity" class="align-middle"></h4>
+              <h4 id="lastRefreshed" class="align-middle"></h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <!-- TradingView Widget BEGIN -->
+              <div class="tradingview-widget-container" id="chart">
+
+              </div>
+              <!-- TradingView Widget END -->
+            </div>
+          </div>`)
+
+    //Generating Currency Name
+    var currencyName = $(this).text();
+    var coinTitle = document.getElementById("coinTitle");
+    var coinSymbol = $(this).attr("id");
+    var imgSrc = "assets/img/icons/color/" + coinSymbol + ".svg";
+
+    console.log(coinSymbol);
+    console.log(currencyName);
+    coinTitle.innerHTML = currencyName + " (" + coinSymbol + ")";
+    $("#img").attr({ src: imgSrc, width: "144rem", height: "144rem" });
+
+    //Generating Health Index
+    var queryURL =
+      "https://www.alphavantage.co/query?function=CRYPTO_RATING&symbol=" + coinSymbol + "&apikey=JIS93MUHML1GONJ2";
+    $.ajax({
+      url: queryURL,
+      dataType: "json",
+      method: "GET"
+    }).then(function (response) {
+      healthRating = response;
+      console.log(healthRating["Crypto Rating (FCAS)"]["3. fcas rating"]);
+
+      var fcasRating = document.getElementById("fcasRating");
+      var marketMaturity = document.getElementById("marketMaturity");
+      var lastRefreshed = document.getElementById("lastRefreshed");
+
+      var fcasRatingValue = healthRating["Crypto Rating (FCAS)"]["3. fcas rating"];
+      var marketMaturityValue = healthRating["Crypto Rating (FCAS)"]["6. market maturity score"];
+      var lastRefreshedValue = healthRating["Crypto Rating (FCAS)"]["8. last refreshed"].split(" ")[0];
+
+
+      fcasRating.innerHTML = "FCAS Rating: " + "<strong>" + fcasRatingValue + "</strong>";
+      marketMaturity.innerHTML = "Market Maturity Score: " + "<strong>" + marketMaturityValue + "</strong>";
+      lastRefreshed.innerHTML = "Last Refreshed: " + "<strong>" + lastRefreshedValue + "</strong>";
+    });
+    $("#chart").empty();
+    $("#chart").append('<div id="tradingview_af9df"></div>')
+
+    new TradingView.widget(
+      {
+        "autosize": true,
+        "symbol": "CRYPTOCAP:" + coinSymbol,
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#333333",
+        "enable_publishing": false,
+        "hide_legend": true,
+        "allow_symbol_change": true,
+        "save_image": false,
+        "container_id": "tradingview_af9df"
+      }
+    );
+
+
   });
 });
